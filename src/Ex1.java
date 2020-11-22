fe83171bc8c7dda8f0b7837000f9a997cb6a1c4b
oribh <bhorib@gmail.com>
2020-11-22 03:11:03 +0200

    add readInput function

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,5 +1,10 @@
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Ex1 {
 
@@ -8,7 +13,72 @@ public class Ex1 {
     }
 
     private static void readInput() throws FileNotFoundException {
-
+        Scanner in = new Scanner(new FileReader("input.txt"));
+//        in.skip("Network");
+        in.nextLine(); // "Network"
+        in.skip("Variables: ");
+//        in.next(); // "Variables:"
+//        in.next();
+//        in.useDelimiter(",");
+        ArrayList<String> variables = new ArrayList<String>(Arrays.asList(in.nextLine().split(",")));
+        System.out.println(variables);
+        in.nextLine(); // Skip the empty line
+        // Read each variable
+        for (int i = 0; i<variables.size(); i++){
+            in.skip("Var ");
+            String name = in.nextLine();
+            in.skip("Values: ");
+            ArrayList<String> values = new ArrayList<String>(Arrays.asList(in.nextLine().split(",")));
+            System.out.println(values);
+            in.skip("Parents: ");
+//            boolean hasParents;
+            ArrayList<String> parents;
+            String parent = in.nextLine();
+            if(parent.equals("none")){
+//                hasParents = false;
+                parents = new ArrayList<String>();
+            }else{
+//                hasParents = true;
+                parents = new ArrayList<String>(Arrays.asList(parent.split(",")));
+                System.out.println(parents);
+            }
+            Variable variable = new Variable(name, values, parents);
+            in.nextLine(); // "CPT:"
+            String entries = "";
+            String entry = "";
+            double probability = 0;
+            // TODO: if there is no parents then the '=' is the first character! I need to take care of that!!
+            while((entries = in.nextLine()).contains("=")){
+                double complementary = 1;
+                String parent_key = "";
+                String self_key = "";
+                if(parents.size()!=0){
+                    parent_key = entries.substring(0, entries.indexOf('=') - 1);
+                }
+                while(entries.contains("=")){
+                    entries = entries.substring(entries.indexOf('=')+1);
+                    if(entries.contains("=")) {
+                        entry = entries.substring(0, entries.indexOf('=') - 1);
+                    }else{
+                        entry = entries;
+                    }
+                    self_key = entry.substring(0, entry.indexOf(','));
+                    probability = Double.parseDouble(entry.substring(entry.indexOf(',')+1));
+                    System.out.println(self_key+","+probability);
+                    variable.addEntry(parent_key,self_key,probability);
+                    complementary-=probability;
+                }
+                for (String value : values) {
+                    HashMap<String, HashMap<String, Double>> cpt = variable.getCpt();
+                    if(!cpt.get(parent_key).containsKey(value))
+                        variable.addEntry(parent_key,value,complementary);
+                }
+//                variable.addEntry(parent_key,"false",complementary);
+//                variable.addEntry(parent_key,self_key,complementary);
+            }
+            variable.printCPT();
+//            break;
+        }
     }
 
     private static void saveToFile(String summary) throws IOException {
@@ -16,7 +86,7 @@ public class Ex1 {
     }
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        readInput();
     }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -4,21 +4,57 @@ import java.util.HashMap;
 
 public class Variable {
     private String name;
-    private ArrayList<String> values_;  //These three attributes below are three disjoint options to store the values of the variable.
-    private HashMap<String, Double> values;
-    private ArrayList<Pair> pair_list_of_values;
-    private ArrayList<Variable> parents;
-    private String cpt_;
-    private HashMap<String, HashMap<ArrayList<Variable>, Double>> cpt;
+    private ArrayList<String> values;  //These three attributes below are three disjoint options to store the values of the variable.
+//    private HashMap<String, Double> values;
+//    private ArrayList<Pair> pair_list_of_values;
+    private boolean hasParents;
+    private ArrayList<String> parents;
+//    private String cpt_;
+    private HashMap<String, HashMap<String, Double>> cpt;
 
-    public Variable(){
-        name ="";
-        values=new HashMap<String, Double>();
-        pair_list_of_values = new ArrayList<Pair>();
-        parents = new ArrayList<Variable>();
-        cpt_ = "";
-        cpt = new HashMap<String, HashMap<ArrayList<Variable>, Double>>();
-//        cpt.
+//    public Variable(){
+//        name ="";
+//        values=new HashMap<String, Double>();
+//        pair_list_of_values = new ArrayList<Pair>();
+//        parents = new ArrayList<Variable>();
+//        cpt_ = "";
+//        cpt = new HashMap<String, HashMap<String, Double>>();
+////        cpt.
+//    }
+
+    public Variable(String name, ArrayList<String> values, ArrayList<String> parents){
+        this.name = name; // Need to change this assignment?
+        this.values = new ArrayList<String>();
+        for (String value : values) {
+            this.values.add(value);
+        }
+        this.parents = new ArrayList<String>();
+        for (String parent : parents) {
+            this.parents.add(parent);
+        }
+        cpt = new HashMap<String, HashMap<String, Double>>();
+    }
+
+    public void addEntry(String parent_key, String self_key, double probability){
+//        HashMap<String, Double> entry = new HashMap<String, Double>();
+//        entry.put(self_key,probability);
+//        cpt.put(parent_key, entry);
+        if(!cpt.containsKey(parent_key))
+            cpt.put(parent_key, new HashMap<String, Double>());
+        cpt.get(parent_key).put(self_key,probability);
+    }
+
+    public HashMap<String, HashMap<String, Double>> getCpt() {
+        return cpt;
+    }
+
+    public void printCPT(){
+        for (String key : cpt.keySet()) {
+            for (String entry : cpt.get(key).keySet()) {
+                System.out.println(key+": "+entry+"="+cpt.get(key).get(entry));
+            }
+        }
+//        System.out.println(cpt);
     }
 
 }
